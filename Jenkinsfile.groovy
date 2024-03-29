67d282a0a909b74a1c082867c47c0babc49eda33
Yasin Isa YILDIRIM <yasin.yildirim@faraday.net>
2024-03-29 14:44:49 +0300

    git parameter type pr

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,20 +3,25 @@ pipeline {
     
     parameters {
         // Parameter that allows us to choose which branch to build
-        gitParameter(branch: '',
-                //branchFilter: 'origin/(release.*)',              // Jenkins will detect and list only release/* branches
-                branchFilter: 'origin/(.*)',                       // Jenkins will detect and list all branches
-                defaultValue: 'release/1.0.0',                      // If we set this value main or master, Jenkins triggers build that branches too
-                description: 'Select branch to build',
-                name: 'github_branch',                      // If everything goes well, this variable will hold something like: release/3.12.3-jenkins
-                quickFilterEnabled: true,                          // It lists the existing branches faster, but it can cause new branches not to be listed.
-                listSize: '20',
-                selectedValue: 'DEFAULT',                          // Default selected value
-                sortMode: 'DESCENDING_SMART',                      // Branch list order by
-                tagFilter: '*',                                    // We don't use this settings but it lists all tags with this situation
-                type: 'PT_BRANCH',                                 // Jenkins will search and detect "branches". It can be search for tags
-                useRepository: 'https://github.com/redsfyre/jenkins-test.git'
-        )
+//        gitParameter(branch: '',
+//                //branchFilter: 'origin/(release.*)',              // Jenkins will detect and list only release/* branches
+//                branchFilter: 'origin/(.*)',                       // Jenkins will detect and list all branches
+//                defaultValue: 'release/1.0.0',                      // If we set this value main or master, Jenkins triggers build that branches too
+//                description: 'Select branch to build',
+//                name: 'github_branch',                      // If everything goes well, this variable will hold something like: release/3.12.3-jenkins
+//                quickFilterEnabled: true,                          // It lists the existing branches faster, but it can cause new branches not to be listed.
+//                listSize: '20',
+//                selectedValue: 'DEFAULT',                          // Default selected value
+//                sortMode: 'DESCENDING_SMART',                      // Branch list order by
+//                tagFilter: '*',                                    // We don't use this settings but it lists all tags with this situation
+//                type: 'PT_BRANCH',                                 // Jenkins will search and detect "branches". It can be search for tags
+//                useRepository: 'https://github.com/redsfyre/jenkins-test.git'
+//        )
+          gitParameter name: 'PULL_REQUESTS',
+                     type: 'PT_PULL_REQUEST',
+                     defaultValue: '1',
+                     sortMode: 'DESCENDING_SMART'
+                     useRepository: 'https://github.com/redsfyre/jenkins-test.git'
     }
  
     stages {

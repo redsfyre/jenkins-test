27bf3f3fe38736cc1c5198cd44e16eafdcddb36e
Furkan Bahceci <fbahceci>
2024-04-15 12:52:56 +0000

    url

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -97,13 +97,13 @@ pipeline {
 }
                     """
                     echo "PAYLOAD: $PAYLOAD"
-                    httpRequest consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'jenkins-event-type', value: 'workflow-completed']], httpMode: 'POST', ignoreSslErrors: true, requestBody: PAYLOAD, responseHandle: 'NONE', url: 'https://orange-plants-dance.loca.lt/api/deployment/d76bfe3c-53c9-4526-bec8-32ba38d7a830', wrapAsMultipart: false
+                    httpRequest consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'jenkins-event-type', value: 'workflow-completed']], httpMode: 'POST', ignoreSslErrors: true, requestBody: PAYLOAD, responseHandle: 'NONE', url: 'https://fine-cups-clean.loca.lt/api/deployment/d76bfe3c-53c9-4526-bec8-32ba38d7a830', wrapAsMultipart: false
                 } else {
                     echo "Build triggered by webhook"
                     def POST_CONTENT = env.POST_CONTENT ?: '{ "status": "failed" }'
                     def PAYLOAD = "$POST_CONTENT"
                     echo "PAYLOAD: $PAYLOAD"
-                    httpRequest consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'jenkins-event-type', value: 'workflow-completed']], httpMode: 'POST', ignoreSslErrors: true, requestBody: PAYLOAD, responseHandle: 'NONE', url: 'https://orange-plants-dance.loca.lt/api/deployment/d76bfe3c-53c9-4526-bec8-32ba38d7a830', wrapAsMultipart: false
+                    httpRequest consoleLogResponseBody: true, contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'jenkins-event-type', value: 'workflow-completed']], httpMode: 'POST', ignoreSslErrors: true, requestBody: PAYLOAD, responseHandle: 'NONE', url: 'https://fine-cups-clean.loca.lt/api/deployment/d76bfe3c-53c9-4526-bec8-32ba38d7a830', wrapAsMultipart: false
                 }
             }
         }
